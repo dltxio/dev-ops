b3797db45d4eec38a7ef96f55183476235a025be
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2022-03-12 08:22:53 +1000

    Chore: update scripts

--- install-dep.sh
+++ install-dep.sh
@@ -4,6 +4,8 @@ cd ~
 curl -sL https://deb.nodesource.com/setup_16.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
 sudo apt install nodejs
+rm nodesource_setup.sh
 
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 sudo apt update && sudo apt install yarn
+

af1d1b35d40172741a51449b094b2a9afa06361e
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2022-03-12 09:54:18 +1000

    Feat: Add domains

--- install-api-only.sh
+++ install-api-only.sh
@@ -17,14 +17,16 @@ cp index.nginx-debian.html /var/www/html/index.nginx-debian.html
 ## Nginx config
 sed 's/api_dltx_io/test.domain.com/g' nginx/default-api-only
 # sudo cp nginx/default-api-only /etc/nginx/sites-available/default
-sudo sed -i 's/api_dltx_io/$1/g' nginx/default-api-only > /etc/nginx/sites-available/default
-
-sed 's/word1/word2/g' input.file
+sudo sed -i 's/api_dltx_io/'$1'/g' nginx/default-api-only > /etc/nginx/sites-available/default
+sudo systemctl reload nginx
 
 sudo ufw allow 22
 sudo ufw allow 'Nginx HTTP'
 sudo ufw allow 'Nginx HTTPS'
 
 sudo npm install pm2@latest -g
+
+sudo apt install certbot python3-certbot-nginx
+sudo certbot --nginx -d $1
 
 sudo ufw enable
